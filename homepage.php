2084182744e2e362b1e30c631abb7357306f5049
Savithri-G <savithri14pt@gmail.com>
2024-08-08 11:51:24 +0530

    Initial commit

---
+++ homepage.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+include("connect.php");
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<style>
+    .homepage{
+        background-color: aquamarine;
+        max-width: 120vw;
+        height: 100vh;
+    }
+    .log
+    {
+        padding: 10px 20px;
+        color: #fff;
+        font-weight: bolder;
+    }
+</style>
+</head>
+<body>
+    <div class="homepage" style="text-align:center; padding:15%;">
+      <p  style="font-size:50px; font-weight:bold;">
+       Hello  <?php 
+       if(isset($_SESSION['email'])){
+        $email=$_SESSION['email'];
+        $query=mysqli_query($conn, "SELECT users.* FROM `users` WHERE users.email='$email'");
+        while($row=mysqli_fetch_array($query)){
+            echo $row['firstName'].' '.$row['lastName'];
+        }
+       }
+       ?>
+       :)
+      </p>
+      <a class = "btn btn-secondary mt-3 log" href="logout.php">Logout</a>
+    </div>
+</body>
+</html>
